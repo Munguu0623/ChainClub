ea3cc0797f7038cfde782bdffd56f72d85ddac3c
Munguu <mungu0623@gmail.com>
2022-03-25 02:31:47 -0700

    buttund animation ugsun link holbov

--- src/components/Cover/navbar/index.js
+++ src/components/Cover/navbar/index.js
@@ -23,9 +23,11 @@ const Navbar = () => {
         </Link> */}
       </ul>
       <div className=" hidden md:w-2/4 lg:w-3/6 md:flex justify-end ">
-        <button className="  mr-16 text-white bg-gradient-to-tr from-buttonColor1 to-buttonColor2 py-1 px-2 rounded-t-3xl rounded-l-3xl text-sm font-normal">
-          Холбоо барих
-        </button>
+        <Link to="/contact">
+          <button className="  mr-16 text-white bg-gradient-to-tr from-buttonColor1 to-buttonColor2 py-1 px-2 rounded-t-3xl rounded-l-3xl text-sm font-normal transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:border-none">
+            Холбоо барих
+          </button>
+        </Link>
       </div>
     </nav>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,16 @@ import { Link } from "react-router-dom";
 import News from "../components/News";
 import HomePage from "./HomePage";
 
-
 export default function Home(props) {
   return (
     <div className="bg-[#1C1C28] w-full h-full font-Roboto z-10">
-      <HomePage/>
+      <HomePage />
       <AboutUs />
       <Team />
       <Info />
       <div className="flex justify-center my-20">
         <Link to="/contact">
-          <button className="border rounded-full text-white font-bold text-3xl h-16 px-11">
+          <button className="border rounded-full text-white font-bold text-3xl h-16 px-11 hover:bg-gradient-to-tr from-buttonColor1 to-buttonColor2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:border-none ">
             Холбоо барих
           </button>
         </Link>
